d52a40dfcbfdd5e7b18f13b1cad95805c89f3be8
dianabasilio <dianabasilio99@gmail.com>
2021-08-31 19:48:46 -0500

    retro y base json

---
+++ src/controllers/productos-json.js
@@ -0,0 +1,11 @@
+const productos = [
+    {
+        productoid: 1,
+        categoria: "anillos",
+        nombreProducto: "Anillo boda 1",
+        descripcion: "Anillo plateado 11k...",
+        imagen: "nombre.png",
+        precio: 1400,
+
+    }
+]
